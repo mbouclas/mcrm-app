c9fc10debcfb6cef0af71bbe22d4d5862fabbeb5
Djole <0xdjole@gmail.com>
2022-10-20 23:25:47 +0200

    type support crud operator

--- src/helpers/crudOperator.ts
+++ src/helpers/crudOperator.ts
@@ -1,32 +1,33 @@
-export const crudOperator = (service, item) => {
-  const parsedItem = cloneItem(item);
+export const crudOperator = <T extends { [key: string]: unknown }>(
+  service,
+  item: T,
+) => {
+  const parsedItem: T = cloneItem(item);
 
   return {
-    create: async () =>  createPage(service, parsedItem),
+    create: async (): Promise<T & { uuid: string }> => createPage(service, parsedItem),
     update: async (item) => updatePage(service, parsedItem.uuid, item),
     delete: async () => deletePage(service, parsedItem),
-    findOne: async () => findOnePage(service, parsedItem.uuid),
-  }
-}
+    findOne: async (): Promise<T> => findOnePage(service, parsedItem.uuid),
+  };
+};
 
-const createPage = async(service, item) => {
+const createPage = async (service, item) => {
   return await service.store(item);
-}
+};
 
-const updatePage = async(service, uuid, item) => {
+const updatePage = async (service, uuid, item) => {
   return await service.update(uuid, item);
-}
+};
 
 const deletePage = async (service, item) => {
   return await service.delete(item.uuid);
-}
+};
 
 const findOnePage = async (service, uuid) => {
-  return await service.findOne({ uuid })
-}
+  return await service.findOne({ uuid });
+};
 
-
-const cloneItem  = (item) => {
+const cloneItem = (item) => {
   return JSON.parse(JSON.stringify(item));
-}
-
+};
