663796b5ba67ce626aeb4827c72a759d55350123
Djole <0xdjole@gmail.com>
2022-11-01 17:47:08 +0100

    status validation and uuid gen

--- src/eshop/order/services/order.service.ts
+++ src/eshop/order/services/order.service.ts
@@ -1,83 +1,95 @@
 import { Injectable } from '@nestjs/common';
-import { ChangeLogService } from "~change-log/change-log.service";
-import { store } from "~root/state";
-import { OnEvent } from "@nestjs/event-emitter";
-import { OrderModel } from "~eshop/order/models/order.model";
-import { BaseNeoService } from "~shared/services/base-neo.service";
-import {  IGenericObject } from "~models/general";
+import { ChangeLogService } from '~change-log/change-log.service';
+import { store } from '~root/state';
+import { OnEvent } from '@nestjs/event-emitter';
+import { OrderModel } from '~eshop/order/models/order.model';
+import { BaseNeoService } from '~shared/services/base-neo.service';
+import { IGenericObject } from '~models/general';
+import { RecordStoreFailedException } from '~shared/exceptions/record-store-failed.exception';
+import { v4 } from 'uuid';
 
 export class OrderModelDto {
   tempUuid?: string;
   uuid?: string;
-  orderId?: string;
   total?: number;
   shippingMethod?: string;
   paymentMethod?: string;
   notes?: string;
-
-  static status: [
-    {
-      id: 1,
-      label: 'started'
-    },
-    {
-      id: 2,
-      label: 'processing'
-    },
-    {
-      id: 3,
-      label: 'shipped' 
-    },
-    {
-      id: 4,
-      label: 'completed'
-    },
-    {
-      id: 5,
-      label: 'cancelled'
-    }
-  ]
-
+  status?: number;
 }
 
 @Injectable()
 export class OrderService extends BaseNeoService {
   protected changeLog: ChangeLogService;
+  statuses: { id: number; label: string }[];
 
   constructor() {
     super();
     this.model = store.getState().models.Order;
 
-
     this.changeLog = new ChangeLogService();
+
+    this.statuses = [
+      {
+        id: 1,
+        label: 'started',
+      },
+      {
+        id: 2,
+        label: 'processing',
+      },
+      {
+        id: 3,
+        label: 'shipped',
+      },
+      {
+        id: 4,
+        label: 'completed',
+      },
+      {
+        id: 5,
+        label: 'cancelled',
+      },
+    ];
   }
 
   @OnEvent('app.loaded')
   async onAppLoaded() {
-    // const s = new OrderService();
-
-    // const r = await s.store({
-    //   orderId: 'orderid12',
-    //   total: 40,
-    //   shippingMethod: 'ship1',
-    //   paymentMethod: 'payment1',
-    //   notes: 'user note'
-    // });
-
-    // console.log(r);
+    // try {
+    //   const s = new OrderService();
+    //   const r = await s.store({
+    //     total: 40,
+    //     shippingMethod: 'ship1',
+    //     paymentMethod: 'payment1',
+    //     notes: 'user note',
+    //     status: 3,
+    //   });
+    //   console.log(r);
+    // } catch (e) {
+    //   console.log(e);
+    // }
   }
 
   async findOne(filter: IGenericObject, rels = []): Promise<OrderModel> {
-    const item = await super.findOne(filter, rels) as unknown as OrderModel;
+    const item = (await super.findOne(filter, rels)) as unknown as OrderModel;
     return item;
   }
 
   async store(record: OrderModelDto, userId?: string) {
-    const r = await super.store(record, userId);
+    const existsStatus = this.statuses.some(
+      (statusItem) => statusItem.id === record.status,
+    );
+
+    if (!existsStatus) {
+      throw new RecordStoreFailedException('Invalid status');
+    }
+
+    const orderId = v4();
+    const r = await super.store({ ...record, orderId }, userId);
     return r;
   }
 
-  async update(uuid:string, record: OrderModelDto, userId?:string) {
+  async update(uuid: string, record: OrderModelDto, userId?: string) {
     const r = await super.update(uuid, record, userId);
 
     return r;
