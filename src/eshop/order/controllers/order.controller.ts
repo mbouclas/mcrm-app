84480cd1447a63bfb768d7dd0b0d322614de59f3
Djole <0xdjole@gmail.com>
2022-11-10 22:28:45 +0100

    order simulation

--- src/eshop/order/controllers/order.controller.ts
+++ src/eshop/order/controllers/order.controller.ts
@@ -1,35 +1,71 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post ,Query} from "@nestjs/common";
-import { IGenericObject } from "~models/general";
-import { OrderService } from "~eshop/order/services/order.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Session,
+} from '@nestjs/common';
+import { IGenericObject } from '~models/general';
+import { OrderService } from '~eshop/order/services/order.service';
+import { CartService } from '~eshop/cart/cart.service';
+import { SessionData } from 'express-session';
 
 @Controller('api/order')
 export class OrderController {
-
-  constructor(
-
-  ) {
-  }
+  constructor() {}
 
   @Get(':uuid')
   async findOne(@Param('uuid') uuid: string, @Query() queryParams = {}) {
-    const rels = (queryParams['with']) ? queryParams['with'] : [];
+    const rels = queryParams['with'] ? queryParams['with'] : [];
 
-    return await (new OrderService()).findOne({ uuid }, rels);
+    return await new OrderService().findOne({ uuid }, rels);
   }
 
   @Patch(`:id`)
-  async update(@Param('id') uuid: string, body: IGenericObject) {
-
-  }
+  async update(@Param('id') uuid: string, body: IGenericObject) {}
 
   @Post()
-  async store(@Body() data: IGenericObject) {
+  async store(@Body() data: IGenericObject) {}
 
+  @Post(`order-simulation`)
+  async orderSimulation(
+    @Session() session: SessionData,
+    @Body() item: IGenericObject,
+  ) {
+    const userId = session.user && session.user['uuid'];
+    let cartItem;
+
+    try {
+      cartItem = await new CartService().createCartItemFromProductId(
+        item.id,
+        item.quantity,
+        item.variantId,
+        userId,
+      );
+    } catch (e) {
+      return { success: false, reason: 'ProductNotFound' };
+    }
+
+    try {
+      session.cart.add(cartItem);
+    } catch (e) {
+      console.log(e);
+    }
+
+    await session.cart.save();
+
+    await new OrderService().store({
+      paymentMethod: item.paymentMethod,
+      shippingMethod: item.shippingMethod,
+    });
+
+    return true;
   }
 
   @Delete()
-  async delete(@Param('id') uuid: string) {
-
-  }
+  async delete(@Param('id') uuid: string) {}
 }
-
