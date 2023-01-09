aef743f2cb3fb6a5af79b3a306ae4b4d43637be5
Djole <0xdjole@gmail.com>
2023-01-09 16:24:35 +0100

    mail service

---
+++ src/mail/Mail.service.ts
@@ -0,0 +1,46 @@
+import { MailgunDriver } from './drivers/mailgun.driver';
+import { SendEmailFailedException } from './exceptions/SendEmailFailed.exception';
+import { SmtpDriver } from './drivers/smtp.driver';
+export interface IBaseMailMessage {
+  from: string;
+  to: string;
+  subject: string;
+  text?: string;
+  html?: string;
+}
+
+export interface IBaseMailServiceDriver {
+  name: string;
+  send(message: IBaseMailMessage): Promise<boolean>;
+}
+
+export class MailService {
+  availableDrivers: { [key: string]: any } = {
+    mailgun: MailgunDriver,
+    smtp: SmtpDriver,
+  };
+
+  driver: IBaseMailServiceDriver;
+
+  constructor(driver?: string) {
+    if (!driver && !process.env.MAIL_DRIVER) {
+      // throw new DriverNotFoundException('No driver found');
+    }
+
+    const key = (driver as string) || (process.env.MAIL_DRIVER as string);
+    if (!this.availableDrivers[key]) {
+      // check the availableDrivers list
+      // throw new DriverNotFoundException(`Driver ${key} is not listed`);
+    }
+
+    this.driver = new this.availableDrivers[key]();
+  }
+
+  async send(message: IBaseMailMessage) {
+    try {
+      await this.driver.send(message);
+    } catch (e) {
+      throw new SendEmailFailedException(e);
+    }
+  }
+}
---
+++ src/mail/drivers/smtp.driver.ts
@@ -0,0 +1,49 @@
+import { IBaseMailMessage, IBaseMailServiceDriver } from '../Mail.service';
+const nodemailer = require('nodemailer');
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+export class SmtpMail implements IBaseMailMessage {
+  from: string;
+  html: string;
+  subject: string;
+  text: string;
+  to: string;
+}
+
+export class SmtpDriver implements IBaseMailServiceDriver {
+  name = 'smtp';
+  client: any;
+
+  constructor() {
+    const poolOptions = {
+      pool: false,
+      maxConnections: 1,
+      maxMessages: 5,
+    };
+
+    const smtpOptions = {
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT as string),
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    };
+
+    const nodemailerOptions: SMTPTransport.Options = {
+      ...poolOptions,
+      ...smtpOptions,
+    };
+
+    this.client = nodemailer.createTransport(nodemailerOptions);
+  }
+
+  async send(message: SmtpMail): Promise<boolean> {
+    try {
+      await this.client.sendMail(message);
+    } catch (e) {}
+
+    return true;
+  }
+}
