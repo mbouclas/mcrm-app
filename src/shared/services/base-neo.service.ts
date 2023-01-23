13a83807e213aec9fdb29ce5f9a098b8e9c7038f
Djole <0xdjole@gmail.com>
2023-01-23 19:05:53 +0100

    retrieve correctly nested

--- src/shared/services/base-neo.service.ts
+++ src/shared/services/base-neo.service.ts
@@ -220,6 +220,38 @@ export class BaseNeoService {
 
     const res = await this.neo.readWithCleanUp(query, {});
 
+    const newRes = res.map((resItem) => {
+      const newResItem = {};
+      for (const modelFieldKey in model.fields) {
+        const modelField = model.fields[modelFieldKey];
+
+        const isFieldNested = modelField.type === 'nested';
+        const modelFieldName = modelField.varName;
+
+        if (isFieldNested) {
+          newResItem[modelFieldName] = {};
+
+          for (const nestedFieldKey in modelField.fields) {
+            const nestedFieldName = modelField.fields[nestedFieldKey].varName;
+            const resNestedKeyName = `${modelFieldName}${capitalizeFirstLetter(
+              nestedFieldName,
+            )}`;
+
+            newResItem[modelFieldName][nestedFieldName] =
+              resItem[resNestedKeyName];
+          }
+        }
+
+        if (!isFieldNested) {
+          newResItem[modelFieldName] = resItem[modelFieldName];
+        }
+
+        resItem[modelFieldName];
+      }
+
+      return newResItem;
+    });
+
     /*    const data = res.records.map(item => {
           const record = item.get(modelAlias).properties;
     
