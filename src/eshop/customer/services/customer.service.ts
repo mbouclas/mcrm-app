e84301e0634cda2e01a515a413a52138b6c1a926
Djole <0xdjole@gmail.com>
2023-05-02 18:51:48 +0200

    customer and pemynet methods init 2

--- src/eshop/customer/services/customer.service.ts
+++ src/eshop/customer/services/customer.service.ts
@@ -5,10 +5,12 @@ import { OnEvent, EventEmitter2 } from '@nestjs/event-emitter';
 import { CustomerModel } from '~eshop/customer/models/customer.model';
 import { BaseNeoService } from '~shared/services/base-neo.service';
 import { IGenericObject, IPagination } from '~models/general';
+import { McmsDiContainer } from '../../../helpers/mcms-component.decorator';
+import { IPaymentMethodProvider } from '~eshop/payment-method/models/providers.types';
 
 export class CustomerModelDto {
   userId?: string;
-  providerCustomerId?: string;
+  email?: string;
   provider?: string;
 }
 
@@ -48,7 +50,26 @@ export class CustomerService extends BaseNeoService {
   }
 
   async store(record: CustomerModelDto, userId?: string) {
-    const r = await super.store(record, userId);
+    const paymentProviderContainer = McmsDiContainer.get({
+      id: `${
+        record.provider.charAt(0).toUpperCase() + record.provider.slice(1)
+      }Provider`,
+    });
+
+    const paymentMethodProvider: IPaymentMethodProvider =
+      new paymentProviderContainer.reference();
+
+    const providerCustomerId = await paymentMethodProvider.createCustomer(
+      record.email,
+    );
+    const r = await super.store(
+      {
+        userId: record.userId,
+        provider: record.provider,
+        customerId: providerCustomerId,
+      },
+      userId,
+    );
 
     return r;
   }
