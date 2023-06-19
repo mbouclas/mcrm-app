e2016980ed78e2bd0e32902a493d9c33533e020a
Djole <0xdjole@gmail.com>
2023-06-19 16:40:15 +0200

    product variant controller

---
+++ src/catalogue/product/controllers/product-variant.controller.ts
@@ -0,0 +1,63 @@
+import { Body, Controller, Post, Session, Get, Delete, Param, Query } from '@nestjs/common';
+import { ProductVariantService } from '~catalogue/product/services/product-variant.service';
+import { IGenericObject } from '~models/general';
+import { SessionData } from 'express-session';
+import { store } from '~root/state';
+import { RecordStoreFailedException } from '~shared/exceptions/record-store-failed.exception';
+
+@Controller('api/product-variant')
+export class ProductVariantController {
+  constructor() { }
+
+  @Get('')
+  async find(@Query() queryParams = {}) {
+    return await new ProductVariantService().find(
+      queryParams,
+      Array.isArray(queryParams['with']) ? queryParams['with'] : [],
+    );
+  }
+
+  @Post('/basic')
+  async storeBasic(@Body() body: IGenericObject) {
+    return await new ProductVariantService().store(body);
+  }
+
+  @Get(':uuid')
+  async findOne(@Param('uuid') uuid: string, @Query() queryParams = {}) {
+    const rels = queryParams['with'] ? queryParams['with'] : [];
+
+    return await new ProductVariantService().findOne({ uuid }, rels);
+  }
+
+  @Delete(':uuid')
+  async delete(@Session() session: SessionData, @Param('uuid') uuid: string) {
+    const userId = session.user && session.user['uuid'];
+
+    return await new ProductVariantService().delete(uuid, userId);
+  }
+
+  @Post('')
+  async create(@Body() body: IGenericObject) {
+    const productVariant = await new ProductVariantService().store(body);
+
+    return { success: true };
+  }
+
+  @Post(':uuid/attach')
+  async addToProductVariant(@Param('uuid') uuid: string, @Body() body: IGenericObject) {
+    const relationships = store.getState().models['ProductVariant'].modelConfig.relationships;
+
+    const targetRelationship = relationships[body.targetModel];
+    if (!targetRelationship) {
+      throw new RecordStoreFailedException('Invalid target model');
+    }
+
+    const response = await new ProductVariantService().attachToModelById(
+      uuid,
+      body.targetId,
+      targetRelationship.modelAlias,
+    );
+
+    return response;
+  }
+}
--- src/catalogue/product/product.module.ts
+++ src/catalogue/product/product.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProductModel } from './models/product.model';
-import { ProductCategoryModel } from "~catalogue/product/models/product-category.model";
-import { SharedModule } from "~shared/shared.module";
+import { ProductCategoryModel } from '~catalogue/product/models/product-category.model';
+import { SharedModule } from '~shared/shared.module';
 import { ProductCategoryService } from './services/product-category.service';
 import { ProductService } from './services/product.service';
-import { ProductVariantModel } from "~catalogue/product/models/product-variant.model";
+import { ProductVariantModel } from '~catalogue/product/models/product-variant.model';
 import { ProductController } from './controllers/product.controller';
 import { ProductVariantService } from './services/product-variant.service';
 import { ProductCategoryController } from './controllers/product-category.controller';
+import { ProductVariantController } from './controllers/product-variant.controller';
 
 @Module({
-  imports: [
-    SharedModule,
-  ],
+  imports: [SharedModule],
   providers: [
     ProductModel,
     ProductCategoryModel,
@@ -20,8 +19,7 @@ import { ProductCategoryController } from './controllers/product-category.contro
     ProductVariantModel,
     ProductService,
     ProductVariantService,
-
   ],
-  controllers: [ProductController, ProductCategoryController]
+  controllers: [ProductController, ProductCategoryController, ProductVariantController],
 })
-export class ProductModule {}
+export class ProductModule { }
