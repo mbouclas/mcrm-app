44fdadc3a3c2a22a16bd0a24a18acf75bc484b2c
Djole <0xdjole@gmail.com>
2023-07-20 20:26:10 +0200

    get customer

--- src/eshop/customer/controllers/customer.controller.ts
+++ src/eshop/customer/controllers/customer.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Query, UseInterceptors } from "@nestjs/common";
-import { UserService } from "~user/services/user.service";
-import { CustomerService } from "~eshop/customer/services/customer.service";
-import { GuestInterceptor } from "~root/auth/interceptors/guest.interceptor";
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
+import { UserService } from '~user/services/user.service';
+import { CustomerService } from '~eshop/customer/services/customer.service';
+import { GuestInterceptor } from '~root/auth/interceptors/guest.interceptor';
 
 @Controller('api/customer')
 export class CustomerController {
   @Get('')
-  @UseInterceptors(GuestInterceptor)
   async find(@Query() queryParams = {}) {
-    return await new UserService().find(queryParams, Array.isArray(queryParams['with']) ? queryParams['with'] : []);;
+    const u = await new UserService().find(queryParams, ['isCustomer']);
+    console.log(u);
+
+    return u;
   }
 }
