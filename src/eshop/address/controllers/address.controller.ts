35448d791f80e45df216c77a43a836cc3d9f8a3c
Djole <0xdjole@gmail.com>
2023-07-26 00:23:03 +0200

    create address

--- src/eshop/address/controllers/address.controller.ts
+++ src/eshop/address/controllers/address.controller.ts
@@ -1,21 +1,21 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-  Session,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Session } from '@nestjs/common';
 import { IGenericObject } from '~models/general';
 import { AddressService } from '~eshop/address/services/address.service';
 import { SessionData } from 'express-session';
+import { IAddress } from '~root/eshop/models/checkout';
+import { IsNotEmpty } from 'class-validator';
+
+export class AddressPostDto {
+  @IsNotEmpty()
+  address: IAddress;
+
+  @IsNotEmpty()
+  userId: string;
+}
 
 @Controller('api/address')
 export class AddressController {
-  constructor() {}
+  constructor() { }
 
   @Get()
   async find(@Session() session: SessionData, @Query() queryParams = {}) {
@@ -33,20 +33,27 @@ export class AddressController {
   }
 
   @Patch(`:uuid`)
-  async update(
-    @Session() session: SessionData,
-    @Param('uuid') uuid: string,
-    @Body() body: IGenericObject,
-  ) {
+  async update(@Session() session: SessionData, @Param('uuid') uuid: string, @Body() body: IGenericObject) {
     const userId = session.user && session.user['uuid'];
 
     return await new AddressService().update(uuid, { ...body, userId });
   }
 
   @Post()
-  async store(@Session() session: SessionData, @Body() body: IGenericObject) {
-    const userId = session.user && session.user['uuid'];
-    return await new AddressService().store({ ...body, userId });
+  async addAddress(@Body() data: AddressPostDto) {
+    if (!AddressService.validateAddress(data.address)) {
+      return { success: false, message: 'Invalid address' };
+    }
+
+    try {
+      const res = await new AddressService().attachAddressToUser(data.address, data.userId);
+      data['uuid'] = res.uuid;
+    } catch (e) {
+      console.log(e);
+      return { success: false, message: e.message, code: e.getCode() };
+    }
+
+    return data;
   }
 
   @Delete(`:uuid`)
