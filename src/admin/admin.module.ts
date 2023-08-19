83c5802b2a4f6aaa80196446f9b1165cb49409ac
Djole <0xdjole@gmail.com>
2023-08-19 22:54:11 +0200

    remove admin gate

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,27 +1,18 @@
-import { Module, OnModuleInit } from "@nestjs/common";
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ModelsService } from './services/models.service';
-import { SharedModule } from "~shared/shared.module";
-import { ModuleRef } from "@nestjs/core";
-import { GatesModel } from "./models/gates.model";
+import { SharedModule } from '~shared/shared.module';
+import { ModuleRef } from '@nestjs/core';
 import { BootController } from './boot.controller';
 import { BootService } from './boot.service';
 
 @Module({
-  providers: [
-    ModelsService,
-    GatesModel,
-    BootService,
-  ],
-  imports: [
-    SharedModule,
-  ],
-  controllers: [BootController]
+  providers: [ModelsService, BootService],
+  imports: [SharedModule],
+  controllers: [BootController],
 })
 export class AdminModule implements OnModuleInit {
   static moduleRef: ModuleRef;
-  constructor(public m: ModuleRef) {
-
-  }
+  constructor(public m: ModuleRef) {}
 
   onModuleInit(): any {
     AdminModule.moduleRef = this.m;
